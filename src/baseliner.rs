8802f85d9ad580517dd46fd89c8d38e1f1934c3b
amaumene <alexandre@maumene.org>
2026-05-12 11:48:17 +0900

    fix: narrow baseliner mutex scope to only cover map mutations
    Release owd_baseline and owd_recent locks before sending metrics
    and debug logging. Locks are now scoped to a block expression that
    returns snapshot values, preventing potential deadlock with other
    threads that also need these maps.

--- src/baseliner.rs
+++ src/baseliner.rs
@@ -60,106 +60,116 @@ impl Baseliner {
             }
             let time_data = self.stats_rx.recv()?;
 
-            let mut owd_baseline_map = self.owd_baseline.lock_anyhow()?;
-            let mut owd_recent_map = self.owd_recent.lock_anyhow()?;
+            // Hold locks only while mutating the maps; release before I/O
+            let (baseline_up_ewma, baseline_down_ewma, recent_up_ewma, recent_down_ewma) = {
+                let mut owd_baseline_map = self.owd_baseline.lock_anyhow()?;
+                let mut owd_recent_map = self.owd_recent.lock_anyhow()?;
 
-            let owd_baseline_new = ReflectorStats {
-                down_ewma: time_data.down_time,
-                up_ewma: time_data.up_time,
-                last_receive_time_s: time_data.last_receive_time_s,
-            };
+                let owd_baseline_new = ReflectorStats {
+                    down_ewma: time_data.down_time,
+                    up_ewma: time_data.up_time,
+                    last_receive_time_s: time_data.last_receive_time_s,
+                };
 
-            let owd_recent_new = ReflectorStats {
-                down_ewma: time_data.down_time,
-                up_ewma: time_data.up_time,
-                last_receive_time_s: time_data.last_receive_time_s,
-            };
+                let owd_recent_new = ReflectorStats {
+                    down_ewma: time_data.down_time,
+                    up_ewma: time_data.up_time,
+                    last_receive_time_s: time_data.last_receive_time_s,
+                };
 
-            let owd_baseline = owd_baseline_map
-                .entry(time_data.reflector)
-                .or_insert(owd_baseline_new);
+                let owd_baseline = owd_baseline_map
+                    .entry(time_data.reflector)
+                    .or_insert(owd_baseline_new);
 
-            let owd_recent = owd_recent_map
-                .entry(time_data.reflector)
-                .or_insert(owd_recent_new);
+                let owd_recent = owd_recent_map
+                    .entry(time_data.reflector)
+                    .or_insert(owd_recent_new);
 
-            if time_data
-                .last_receive_time_s
-                .duration_since(owd_baseline.last_receive_time_s)
-                .as_secs_f64()
-                > 30.0
-                || time_data
+                if time_data
                     .last_receive_time_s
-                    .duration_since(owd_recent.last_receive_time_s)
+                    .duration_since(owd_baseline.last_receive_time_s)
                     .as_secs_f64()
                     > 30.0
-            {
-                owd_baseline.down_ewma = time_data.down_time;
-                owd_baseline.up_ewma = time_data.up_time;
+                    || time_data
+                        .last_receive_time_s
+                        .duration_since(owd_recent.last_receive_time_s)
+                        .as_secs_f64()
+                        > 30.0
+                {
+                    owd_baseline.down_ewma = time_data.down_time;
+                    owd_baseline.up_ewma = time_data.up_time;
+                    owd_baseline.last_receive_time_s = time_data.last_receive_time_s;
+                    owd_recent.down_ewma = time_data.down_time;
+                    owd_recent.up_ewma = time_data.up_time;
+                    owd_recent.last_receive_time_s = time_data.last_receive_time_s;
+                }
+
                 owd_baseline.last_receive_time_s = time_data.last_receive_time_s;
-                owd_recent.down_ewma = time_data.down_time;
-                owd_recent.up_ewma = time_data.up_time;
                 owd_recent.last_receive_time_s = time_data.last_receive_time_s;
-            }
 
-            owd_baseline.last_receive_time_s = time_data.last_receive_time_s;
-            owd_recent.last_receive_time_s = time_data.last_receive_time_s;
+                // if this reflection is more than 5 seconds higher than baseline... mark it no good and trigger a reselection
+                if time_data.up_time > owd_baseline.up_ewma + 5000.0
+                    || time_data.down_time > owd_baseline.down_ewma + 5000.0
+                {
+                    // mark the data as bad by setting the receive time to the time autorate was started
+                    owd_baseline.last_receive_time_s = self.start_time;
+                    owd_recent.last_receive_time_s = self.start_time;
+                    info!(
+                        "Reflector {} has OWD > 5 seconds more than baseline, triggering reselection",
+                        time_data.reflector
+                    );
+                    self.event_metrics.send(Metric::Event {
+                        name: "reselection",
+                        reason: "anomaly",
+                        reflector: Some(time_data.reflector),
+                        tags: &[],
+                    });
+                    // If reselection is disabled this would trigger an error
+                    // so just ignore the result
+                    let _ = self.reselect_trigger.send(true);
+                } else {
+                    owd_baseline.down_ewma = owd_baseline.down_ewma * slow_factor
+                        + (1.0 - slow_factor) * time_data.down_time;
+                    owd_baseline.up_ewma =
+                        owd_baseline.up_ewma * slow_factor + (1.0 - slow_factor) * time_data.up_time;
 
-            // if this reflection is more than 5 seconds higher than baseline... mark it no good and trigger a reselection
-            if time_data.up_time > owd_baseline.up_ewma + 5000.0
-                || time_data.down_time > owd_baseline.down_ewma + 5000.0
-            {
-                // mark the data as bad by setting the receive time to the time autorate was started
-                owd_baseline.last_receive_time_s = self.start_time;
-                owd_recent.last_receive_time_s = self.start_time;
-                info!(
-                    "Reflector {} has OWD > 5 seconds more than baseline, triggering reselection",
-                    time_data.reflector
-                );
-                self.event_metrics.send(Metric::Event {
-                    name: "reselection",
-                    reason: "anomaly",
-                    reflector: Some(time_data.reflector),
-                    tags: &[],
-                });
-                // If reselection is disabled this would trigger an error
-                // so just ignore the result
-                let _ = self.reselect_trigger.send(true);
-            } else {
-                owd_baseline.down_ewma = owd_baseline.down_ewma * slow_factor
-                    + (1.0 - slow_factor) * time_data.down_time;
-                owd_baseline.up_ewma =
-                    owd_baseline.up_ewma * slow_factor + (1.0 - slow_factor) * time_data.up_time;
+                    owd_recent.down_ewma =
+                        owd_recent.down_ewma * fast_factor + (1.0 - fast_factor) * time_data.down_time;
+                    owd_recent.up_ewma =
+                        owd_recent.up_ewma * fast_factor + (1.0 - fast_factor) * time_data.up_time;
 
-                owd_recent.down_ewma =
-                    owd_recent.down_ewma * fast_factor + (1.0 - fast_factor) * time_data.down_time;
-                owd_recent.up_ewma =
-                    owd_recent.up_ewma * fast_factor + (1.0 - fast_factor) * time_data.up_time;
+                    if owd_baseline.down_ewma > owd_recent.down_ewma {
+                        owd_baseline.down_ewma = owd_recent.down_ewma;
+                    }
 
-                if owd_baseline.down_ewma > owd_recent.down_ewma {
-                    owd_baseline.down_ewma = owd_recent.down_ewma;
+                    if owd_baseline.up_ewma > owd_recent.up_ewma {
+                        owd_baseline.up_ewma = owd_recent.up_ewma;
+                    }
                 }
 
-                if owd_baseline.up_ewma > owd_recent.up_ewma {
-                    owd_baseline.up_ewma = owd_recent.up_ewma;
-                }
-            }
+                (
+                    owd_baseline.up_ewma,
+                    owd_baseline.down_ewma,
+                    owd_recent.up_ewma,
+                    owd_recent.down_ewma,
+                )
+            }; // locks released here
 
             self.baseline_metrics.send(Metric::Baseline {
                 reflector: time_data.reflector,
-                baseline_up_ewma: owd_baseline.up_ewma,
-                baseline_down_ewma: owd_baseline.down_ewma,
-                recent_up_ewma: owd_recent.up_ewma,
-                recent_down_ewma: owd_recent.down_ewma,
+                baseline_up_ewma,
+                baseline_down_ewma,
+                recent_up_ewma,
+                recent_down_ewma,
             });
 
             debug!(
                 "Reflector {} up baseline = {} down baseline = {}",
-                time_data.reflector, owd_baseline.up_ewma, owd_baseline.down_ewma
+                time_data.reflector, baseline_up_ewma, baseline_down_ewma
             );
             debug!(
                 "Reflector {} up recent = {} down recent = {}",
-                time_data.reflector, owd_recent.up_ewma, owd_recent.down_ewma
+                time_data.reflector, recent_up_ewma, recent_down_ewma
             );
         }
     }
